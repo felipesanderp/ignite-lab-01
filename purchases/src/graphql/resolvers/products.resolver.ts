2efa96a467f7aae60c6bd4024fcf5799d53b0a76
Felipe Sander <felipe.panisson@hotmail.com>
2022-08-03 16:54:43 -0300

    create query and mutation to handle products

---
+++ purchases/src/graphql/resolvers/products.resolver.ts
@@ -0,0 +1,24 @@
+import { UseGuards } from '@nestjs/common';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+
+import { ProductsService } from '../../services/products.service';
+import { AuthorizationGuard } from '../../http/auth/authorization.guard';
+import { Product } from '../models/product';
+import { CreateProductInput } from '../inputs/create-product-input';
+
+@Resolver()
+export class ProductsResolver {
+  constructor(private productsService: ProductsService) { }
+
+  @Query(() => [Product])
+  // @UseGuards(AuthorizationGuard)
+  products() {
+    return this.productsService.listAllProducts();
+  }
+
+  @UseGuards(AuthorizationGuard)
+  @Mutation(() => Product)
+  createProduct(@Args('data') data: CreateProductInput) {
+    return this.productsService.createProduct(data);
+  }
+}
