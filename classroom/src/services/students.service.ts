ce11a830d53148edc074af7efbf6787185be3160
Felipe Sander <felipe.panisson@hotmail.com>
2022-08-16 15:54:09 -0300

    create query to list all students

--- classroom/src/services/students.service.ts
+++ classroom/src/services/students.service.ts
@@ -4,4 +4,8 @@ import { PrismaService } from '../database/prisma/prisma.service';
 @Injectable()
 export class StudentsService {
   constructor(private prisma: PrismaService) {}
+
+  listAllStudents() {
+    return this.prisma.student.findMany();
+  }
 }
